53fbd1068b83d1c39ee9a2bc39aba57b0379ebbe
asilberschneider <a.silberschneider@uppernetwork.com>
2015-09-21 23:12:57 +0200

    Added code for pagination, filtering and partial responses
    Added code for pagination of collection results and filtering, added
    samples, added sample on how to implement partial response fields

---
+++ MailChimpTest/CustomMailChimpManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using MailChimp;
+using MailChimp.DTOs;
+using RestSharp;
+
+namespace MailChimpTest
+{
+    class CustomMailChimpManager: MailChimpManager
+    {
+        public CustomMailChimpManager(string accessToken, string datacenter) : base(accessToken, datacenter)
+        {
+        }
+        public CustomMailChimpManager(string apiKey)
+            : base(apiKey)
+        {
+        }
+
+        /// <summary>
+        /// Get templates with just the id and name properties filled
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public TemplatesCollection GetTemplatesLight(int? offset = null, int? count = null, TemplatesInstance filter = null)
+        {
+            try
+            {
+                return MakeAPICall<TemplatesCollection>("templates", Method.GET, null, offset, count, filter, new List<string>
+                {
+                    "templates.id", "templates.name"
+                });
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
